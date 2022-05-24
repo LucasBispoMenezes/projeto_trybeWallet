132ff12115cb4d9030489f7d17eda10913291f46
Lucas Bispo Menezes <skaliboo1@gmail.com>
2022-05-24 10:15:49 -0300

    resolvendo Problemas

--- src/redux/actions/index.js
+++
@@ -1,9 +0,0 @@
-// Coloque aqui suas actions
-export const ADD_USER_TO_STATE = 'ADD_USER_TO_STATE';
-
-const addUserToState = (user) => ({
-  type: ADD_USER_TO_STATE,
-  user,
-});
-
-export default addUserToState;
